49344e88db59ae393c6c37bc536ae1ead129b0ed
Jenny Aguilar <contact@jaguilarweb.com>
2021-10-11 16:25:28 -0300

    refactor: Change table as a Table component

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Spinner from '../General/Spinner';
 import Fatal from '../General/Fatal';
+import Tabla from './Tabla';
 import * as usuariosActions from '../../actions/usuariosActions';
 
 class Usuarios extends Component {
@@ -18,37 +19,10 @@ class Usuarios extends Component {
     if (this.props.error){
       return <Fatal mensaje={this.props.error} />;
     }
-    return(
-      <table className='tabla'>
-        <thead>
-          <tr>
-            <th>
-              Nombre
-            </th>
-            <th>
-              Correo
-            </th>
-            <th>
-              Enlace
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          { this.ponerFilas() }
-        </tbody>
-      </table>
-    )
+    return <Tabla usuarios={this.props.usuarios}/>
   }
 
-  ponerFilas = () => (
-    this.props.usuarios.map((usuario) => (
-      <tr key={usuario.id}>
-        <td>{usuario.name}</td>
-        <td>{usuario.email}</td>
-        <td>{usuario.website}</td>
-      </tr>
-    ))
-  );
+
 
   render(){
     console.log(this.props.cargando);
