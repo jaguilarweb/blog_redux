18e09380fa3d1950f9b543a313ee22f7331d5f3c
Jenny Aguilar <contact@jaguilarweb.com>
2021-10-11 10:43:44 -0300

    chore: Add react-router-dom package

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,18 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Menu from './Menu';
+import Usuarios from './Usuarios';
+
+const Tareas = () => <div>Tareas</div>
+
+const App = () => (
+  <BrowserRouter>
+    <Menu />
+    <Route exact path='/' component={ Usuarios } />
+    <Route exact path='/tareas' component={ Tareas } />
+  </BrowserRouter>
+);
 
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      usuarios: []
-    }
-  }
-
-  async componentDidMount(){
-    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users')
-    this.setState({
-      usuarios: respuesta.data
-    })
-  }
-
-  ponerFilas = () => (
-    this.state.usuarios.map((usuario) => (
-      <tr key={usuario.id}>
-        <td>{usuario.name}</td>
-        <td>{usuario.email}</td>
-        <td>{usuario.website}</td>
-      </tr>
-    ))
-  );
-
-  render(){
-    console.log(this.state.usuarios)
-    return(
-      <div className='margen'>
-        <table className='tabla'>
-          <thead>
-            <tr>
-              <th>
-                Nombre
-              </th>
-              <th>
-                Correo
-              </th>
-              <th>
-                Enlace
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.ponerFilas() }
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-
-}
 
 export default App;
